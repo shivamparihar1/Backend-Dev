f1452ed3bddee734444ecc87f72575f882d4b1ba
Your Name <your_email@example.com>
2026-02-13 23:48:29 +0530

    class task => Built student registration feature using Express & EJS

--- Expressjs-backend/server3.js
+++ Expressjs-backend/server3.js
@@ -1,42 +1,56 @@
+const fs = require("fs").promises;
 const express = require("express");
 const app = express();
-const fs = require("fs").promises;
 
-app.set("view engine", "ejs");
+app.set("view engine","ejs");
 
+app.use(express.urlencoded({extended:true}));
 
-app.use(express.urlencoded({extended: true}));
-const students = [
-    {name: "Alice", branch: "CSE"},
-    {name: "Bob", branch: "Civil"},
-    {name: "Charlie", branch: "Mechanical"}
-];
+const readStudentsFromFile = async () => {
+  const data = await fs.readFile("./students.json", "utf-8");
+  console.log(data)
+  return JSON.parse(data || "[]");
+};
 
+const writeStudentsToFile = async (records) => {
+  await fs.writeFile("./students.json", JSON.stringify(records, null, 2));
+};
 
-// const readStudentsFromFile = async () => {
-//   const data = await fs.readFile("./students.json", "utf-8");
-//   return JSON.parse(data || "[]");
-// };
+app.get("/", async(req, res) => {
+    const filedata= await readStudentsFromFile();
+    console.log(filedata);
+    res.render("form",{allStudents:filedata});
+}) 
 
-// const writeStudentsToFile = async (records) => {
-//   await fs.writeFile("./students.json", JSON.stringify(records, null, 2));
-// };
+app.post("/Students/register", async (req, res)=>{
+    const {name, branch} = req.body;
+    if(!name || !branch){
+        return res.status(400).send("Invalid student data");
+    }
+    console.log(name)
 
+    try {
+        const students = await readStudentsFromFile();
+        console.log(students)
+        const newStudent = {
+            id: students.length > 0 ? students[students.length - 1].id + 1 : 1,
+            name,
+            branch,
+        };
+        students.push(newStudent);
 
-
-app.get("/",(req, res) => {
-    // const fileData = await readStudentsFromFile();
-    // console.log(fileData);
-    res.render("form", { allStudents: students });
+        await writeStudentsToFile(students);
+        // only send response after successfully writing to the file
+        res.status(201).json({message: "Student registered successfully", student: newStudent });
+    } catch (err) {
+        res.status(500).send("Error processing request");
+    }
 });
 
-app.post("/students/register", (req, res) => {
-    console.log("form data:", req.body);
-    students.push(req.body);
-    res.redirect("/");
-    
-});
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-})
+
+
+const PORT= 3000;
+app.listen(PORT, () => {
+  console.log("Server is listening on port:3000");
+});
